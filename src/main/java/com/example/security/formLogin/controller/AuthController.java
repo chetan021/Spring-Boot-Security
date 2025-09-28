4ddbca06a872ddcb2becda52d89a107ea7a41c5e
unknown <cj172080@gmail.com>
2025-09-28 12:37:13 +0530

    first commit : Learned about the inbuilt and custom Login and Logout features in Spring Boot

---
+++ src/main/java/com/example/security/formLogin/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.security.formLogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "Login";
+    }
+
+}
